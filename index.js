e5944b310743751af650ec187185578499d3323a
Alen <alen.thomas@calpinetech.com>
2025-03-07 11:49:30 +0530

    Listening port changed to 3009.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
-const port = 3000;
+const port = 3009;
 app.use(express.static(path.join(__dirname, "assets")));
 app.set('view engine', 'ejs');
 
